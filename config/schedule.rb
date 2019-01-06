e04be987bbbab1393d17f5f4fb7f09a22a74c0b9
shogo takayama <shogo@chalkboard.me>
2019-01-06 14:33:13 +0900

    cron改修

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,12 +6,11 @@ rails_env = ENV['RAILS_ENV'] || :development
 ENV.each { |k, v| env(k, v) }
 set :environment, rails_env
 # UTCです。(- 9:00)
-# 7:00, 10:00, 16:00, 22:00
+# 16:00, 19:00, 23:00, 7:00
 every '0 7,10,14,21 * * *' do
   rake "import_tweets:exec"
 end
 
-# 7:30, 10:30, 16:30, 22:30
 every '30 7,10,14,21 * * *' do
   rake "choice_category:exec"
 end
